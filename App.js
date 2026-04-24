51ae5f2f0910ab7fe2759199d3d7320f3c30db04
kyg <kygljm@gamil.com>
2026-04-24 22:27:12 +0900

    add text style

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 
 export default function App() {
-  const [number, setNumber] =useState(0);
+  const [number, setNumber] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 50}}>result : {number} </Text>
-      <View style={{flexDirection:"row", gap: 10}}>
-        <Button title='up' onPress={() => setNumber(number+1)}/>
-        <Button title='down' onPress={() => setNumber(number-1)}/>
+      <Text style={{...styles.text, color: "blue"}}>result : {number} </Text>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Button title='up' onPress={() => setNumber(number + 1)} />
+        <Button title='down' onPress={() => setNumber(number - 1)} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -22,8 +22,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: "red",
+
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "red",
   },
 });
